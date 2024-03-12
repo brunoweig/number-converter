7efa5e4e7d115d1789e2887c6e8f3dab5556f6be
Bruno Weigmann de Matos <mouts.bruno@tigre.com>
2024-03-12 10:17:28 -0300

    RomanConverter testes

--- test/RomanConverter.test.js
+++ test/RomanConverter.test.js
@@ -3,14 +3,22 @@ import { RomanConverter } from "../src/domain/RomanConverter"
 describe('First', () => {
     const converter = new RomanConverter()
 
-    const values = [
+    const valid_cases = [
         ['I', 1],
         ['VI', 6],
         ['IV', 4],
         ['IX', 9],
+        ['LX', 60],
+        ['XL', 40],
+        ['CC', 200],
+        ['XC', 90],
+        ['DC', 600],
+        ['CD', 400],
+        ['MM', 2000],
+        ['CM', 900],
     ]
 
-    it.each(values)('units %s = %d', (input, expected) => {
+    it.each(valid_cases)('valid %s = %d', (input, expected) => {
         expect(converter.get(input)).toBe(expected)
     })
 });
