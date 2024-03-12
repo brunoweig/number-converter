3ddb3f7862a2b87e73148bd3b8da0e2c398e07c5
Bruno Weigmann de Matos <mouts.bruno@tigre.com>
2024-03-12 18:33:13 -0300

    Exibindo erro na tela

--- src/ui/main.js
+++ src/ui/main.js
@@ -1,3 +1,7 @@
+import { RomanConverter } from "../domain/RomanConverter.js"
+
+const converter = new RomanConverter()
+
 const input = document.getElementById('input')
 const target = document.getElementById('target')
 
@@ -7,5 +11,10 @@ input.addEventListener('keyup', onKeyPress)
  * @param {KeyboardEvent} e 
  */
 function onKeyPress(e) {
-    target.innerText = input.value;
+    try {
+        const result = converter.get(input.value)
+        target.innerText = result
+    } catch (e) {
+        target.innerText = 'Invalid number'
+    }
 }
